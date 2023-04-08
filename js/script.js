82b813c326f227cc5bbb34dc920b581b94b04a6e
Arg.Prog.Grupo12 <argentinaprograma.grupo12@gmail.com>
2023-04-08 16:32:12 -0300

    Se agrego la funcionalidad del carrito de compras, se pueden agregar elementos al carrito y visualizar el carrito haciendo click en el globo del carrito. Falta pulir diseño y falta eliminar item del carrito.

--- js/script.js
+++ js/script.js
@@ -77,13 +77,70 @@ const mostrarCarritoCompras = () => {
             <span class="textoTituloVentana">CARRITO DE COMPRAS</span>
         </div>
         <div class="contenidoVentana">
-            TODAVIA EN CONSTRUCCION
+            <table class="tablaCarrito">
+                <thead>
+                    <tr>
+                        <th scope="col">Cant.</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Total</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody id="tablaCarrito" id="tablaCarrito">
+                </tbody>
+            </table>
         </div>
-        <div class="pieVentana">
-            <input type="button" id="botonCancelar" class="boton btnCerrar pointer" onclick="toggleVentana()" value="Cerrar">
-        </div>
-
     ` 
+    const tablaElementosCarrito = document.getElementById('tablaCarrito')
+    for (item of carrito.listadoProductos) {
+        tablaElementosCarrito.innerHTML += `
+                <th scope='row'>${item.cantidad}</th>
+                <td>${item.nombre}</td>
+                <td>${item.precio}</td>
+                <td>${item.precio * item.cantidad}</td>
+                <td><i class="bi bi-trash3 pointer" onclick="eliminarItemCarrito(${item.id})"></i></td>
+            </tr>
+        `
+    }
+    ventana.innerHTML += `
+        <div class="pieCarrito">
+            <div class="cantidadItems">
+                <span class="">Cantidad de Items</span>
+                <span class="">${carrito.cantidadItems}</span>
+            </div>
+            <div class="totalPagar">
+                <span class="">Total a Pagar</span>
+                <span class="">${carrito.totalPagar}</span>
+            </div>
+        </div>
+    <div class="pieVentana">
+        <input type="button" id="botonCancelar" class="boton btnCerrar pointer" onclick="toggleVentana()" value="Cerrar">
+    </div>
+
+    `
+}
+
+const agregarProductoCarrito = (id, cantidad, nombre, precio) => {
+    const existeProducto = carrito.listadoProductos.find(producto => producto.id == id)
+    const inputCantidad = document.getElementById('cantidadId'+id)
+    if (existeProducto) { 
+        existeProducto.cantidad += parseInt(inputCantidad.value)
+        existeProducto.total = existeProducto.cantidad * existeProducto.precio
+        carrito.cantidadItems += parseInt(inputCantidad.value)
+        carrito.totalPagar += precio * parseInt(inputCantidad.value)
+    } else {
+        const producto = {
+            id: id,
+            nombre: nombre,
+            precio: precio,
+            cantidad: parseInt(inputCantidad.value),
+            total: precio * parseInt(inputCantidad.value)
+        }
+        carrito.cantidadItems += parseInt(inputCantidad.value)
+        carrito.totalPagar += precio * parseInt(inputCantidad.value)
+        carrito.listadoProductos.push(producto)
+    }
 }
 
 const mostrarDatosProducto = (id) => {
@@ -94,21 +151,25 @@ const mostrarDatosProducto = (id) => {
     const ventana = document.getElementById('contenidoVentanaEmergente')
     ventana.innerHTML=`
         <form action="">
-                    <div class="tituloVentana" >
-                        <span class="textoTituloVentana">Detalle del Producto</span>
-                    </div>
-                    <div class="contenidoVentana">
-                        <span class="nombreProductoVentana">${producto.nombre}</span>
-                        <div class="imagenDescripcion">
-                            <img class="imagenProductoVentana" src="${producto.foto}" alt="">
-                            <p class="textoDescripcion">${producto.beneficios}</p>
-                        </div>
-                        <p class="precioProductoVentana" ><b>Precio: </b>$${producto.precio}</p>
-                    </div>
-                    <div class="pieVentana">
-                        <input type="button" id="botonCancelar" class="boton btnCerrar pointer" onclick="toggleVentana()" value="Cerrar">
-                    </div>
-                </form>
+            <div class="tituloVentana" >
+                <span class="textoTituloVentana">Detalle del Producto</span>
+            </div>
+            <div class="contenidoVentana">
+                <span class="nombreProductoVentana">${producto.nombre}</span>
+                <div class="imagenDescripcion">
+                    <img class="imagenProductoVentana" src="${producto.foto}" alt="">
+                    <p class="textoDescripcion">${producto.beneficios}</p>
+                </div>
+                <div class="cantidadCarrito">
+                    <p class="precioProductoVentana" ><b>Precio: </b>$${producto.precio}</p>
+                    <input type="number" class="campo" min="1" max="10" id="cantidadId${producto.id}" value="1" style="text-align: center; font-weight: 600;">
+                    <input type="button" id="botonAgregar" class="boton btnAgregar pointer" onclick="agregarProductoCarrito(${producto.id},1,'${producto.nombre}',${producto.precio})" value="Agregar al Carrito">
+                </div>
+            </div>
+            <div class="pieVentana">
+                <input type="button" id="botonCancelar" class="boton btnCerrar pointer" onclick="toggleVentana()" value="Cerrar">
+            </div>
+        </form>
     ` 
 }
 
