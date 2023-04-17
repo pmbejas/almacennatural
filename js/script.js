f90c72be5e9d1b11d2ab50b6ed18d29ee06e5a95
Arg.Prog.Grupo12 <argentinaprograma.grupo12@gmail.com>
2023-04-16 23:58:07 -0300

    Correcciones menores. Se agrego un sistema de Login con 2 usuarios cargados en json

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 let productos = []
+let usuarioActivo = {
+    id: 0,
+    nombre: "",
+    apellido: "",
+    email:"",
+    avatar: 0
+}
+
 let carrito = {
     fecha: Date.now,
     cantidadItems: 0,
@@ -12,7 +20,107 @@ const mostrarMenuMovil = () => {
     menu.classList.toggle('ocultar')
 }
 
+const mostrarUsuarioActivo = () => {
+    toggleElemento('cardUsuario','add')
+    toggleElemento('cardUsuarioInactivo','add')
+
+    if (usuarioActivo.id > 0) {
+        toggleElemento('iconoUsuarioActivo', 'remove')
+        toggleElemento('iconoUsuarioInactivo', 'add')
+
+        const nombre= document.getElementById('nombreUsuario')
+        nombre.innerHTML = usuarioActivo.nombre + " " + usuarioActivo.apellido;
+        const email = document.getElementById('emailUsuario')
+        email.innerHTML = usuarioActivo.email;
+        const foto = document.getElementById('iconoUsuarioActivo')
+        foto.src = './img/usuarios/usuarioId'+usuarioActivo.id+'.jpg'
+
+    } else {
+        toggleElemento('iconoUsuarioActivo', 'add')
+        toggleElemento('iconoUsuarioInactivo', 'remove')
+    }
+}
+
+const logOut = () => {
+    usuarioActivo = {
+        id: 0,
+        nombre: "",
+        apellido: "",
+        email:"",
+        avatar: 0
+    }
+    mostrarUsuarioActivo()
+}
+
+const logIn = () => {
+    
+    toggleElemento('cardUsuarioInactivo','add')
+    toggleVentana()
+
+    const ventana = document.getElementById('contenidoVentanaEmergente')
+    ventana.innerHTML=`
+            <div class="tituloVentana" >
+                <span class="textoTituloVentana">Formulario de LogIn</span>
+                <i class="bi bi-x-lg pointer botonCerrarVentana" id="botonCancelar" onclick="toggleVentana()" ></i>
+            </div>
+            <div class="contenidoVentana">
+                <div class="login">
+                    <form class="formLogin" action="" id="formLogin">
+                        <span class"tituloFormLogin">Acceso al Sistema</span>
+                        <div class="lineaFormulario">
+                            <label for="email">Email:</label>
+                            <input class="campoUsuario" type="email" name="email" id="campoUsuario" required>
+                        </div>
+                        <div class="lineaFormulario">
+                            <label for="contraseña">Contraseña:</label>
+                            <input class="campoUsuario" type="password" name="contrasena" id="campoContrasena" required>
+                        </div>
+                        <div class="lineaFormulario">
+                            <button class="boton btnAgregar" type="submit" onclick="identificarse()">Identificarse</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            `
+}
+
+const identificarse = () => {
+    const formulario = document.getElementById('formLogin');
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        fetch("./db/usuarios.json")
+        .then( res => res.json() )
+        .then( usuarios => {
+            let usuarioSeleccionado  = usuarios.find(usuario => usuario.email === formulario.email.value); 
+            if (usuarioSeleccionado) {
+                if (usuarioSeleccionado.contrasena === formulario.contrasena.value) {
+                    usuarioActivo = {
+                        id: usuarioSeleccionado.id,
+                        nombre: usuarioSeleccionado.nombre,
+                        apellido: usuarioSeleccionado.apellido,
+                        email: usuarioSeleccionado.email,
+                        avatar: 1
+                    }
+                    toggleVentana()
+                    mostrarUsuarioActivo()
+                    alerta('celeste','Acceso Correcto', 2000)
+                } else {
+                    alerta('rojo','Contraseña Incorrecta', 2000)
+                }
+            } else {
+                alerta('rojo','Email NO registrado', 2000)
+            }
+        });
+    })
+
+}
+
 const mostrarPagina = (eleccion) => {
+
+    const elementoToggle = document.getElementById('cardUsuario')
+    elementoToggle.classList.add('ocultar')
+
     let bloques = document.getElementsByClassName('mainContent')
     for (const bloque of bloques) {
         bloque.classList.add('ocultar')
@@ -73,17 +181,32 @@ const mostrarProductos = (url, elemento) => {
 }
 
 
-const toggleVentana = () => {
-    
-    const cuerpo = document.getElementById('cuerpo')
-    cuerpo.classList.toggle('scroll')
+const toggleElemento = (elemento, accion) => {
+    const elementoClase = document.getElementById(elemento)
+    switch (accion) {
+        case 'add':
+            elementoClase.classList.add('ocultar')        
+            break;
+        case 'toggle':
+            elementoClase.classList.toggle('ocultar')        
+            break;
+        case 'remove':
+            elementoClase.classList.remove('ocultar')        
+            break;
+    }
+}
 
-    const ventana = document.getElementById('ventanaEmergente')
-    ventana.classList.toggle('ocultar')
+const toggleVentana = () => {
+
+    const elementoToggle = document.getElementById('cardUsuario')
+    elementoToggle.classList.add('ocultar')
+
+    toggleElemento('ventanaEmergente', 'toggle')
 
     const globo = document.getElementById('globoCarrito')
     globo.classList.toggle('ocultar')
 }
+
 
 function guardarCarritoLocal () {
     carritoLocal.setItem('carrito', JSON.stringify(carrito));
@@ -209,6 +332,7 @@ const buscarProducto = (seccion) => {
         } 
 }
 
+
 const mostrarDatosProducto = (id) => {
     toggleVentana()
     const producto = productos.find((productoBuscado) => {
@@ -265,7 +389,13 @@ const alerta = (fondo, texto, tiempo) => {
         case 'celeste':
             ventana.classList.add('fondoCeleste')
             break;
-    }
+        case 'rojo':
+            ventana.classList.add('fondoRojo')
+            break;
+        case 'rojoClaro':
+            ventana.classList.add('fondoRojoClaro')
+            break;
+        }
     setTimeout(() => {
         ventana.classList.add('opacidad1')
       }, 10);
@@ -282,8 +412,57 @@ const alerta = (fondo, texto, tiempo) => {
       }, tiempo+1000);
 }
 
+const cambioScreenSize = () => {
+    let agregarHTML
+    if (document.documentElement.clientWidth < 1024) {
+        agregarHTML = document.getElementById('divUsuario')
+        agregarHTML.innerHTML = ""    
+        agregarHTML = document.getElementById('usuarioMovil')
+    } else {
+        agregarHTML = document.getElementById('usuarioMovil')
+        agregarHTML.innerHTML = ""    
+        agregarHTML = document.getElementById('divUsuario')
+    }
+    agregarHTML.innerHTML = `
+        <img class="fotoUsuario pointer" id="iconoUsuarioActivo" src="" alt="" onclick="toggleElemento('cardUsuario','toggle')">
+        <div class="cardUsuario ocultar" id="cardUsuario">
+            <img class="cardFotoUsuario" src="./img/usuarios/usuarioId1.jpg" alt="">
+            <h4 id="nombreUsuario"></h4>
+            <span id="emailUsuario"></span>
+            <div class="separador"></div>
+            <div class="items pointer">
+                <div class="itemUsuario" onclick="logOut()">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span class="">Salir</span>
+                </div>
+            </div>
+        </div>
+        <img class="fotoUsuario pointer ocultar" id="iconoUsuarioInactivo" src="./img/usuarios/userBlanco.png" alt="" onclick="toggleElemento('cardUsuarioInactivo','toggle')">
+        <div class="cardUsuario ocultar" id="cardUsuarioInactivo">
+            <img class="cardFotoUsuario" src="./img/usuarios/userBlanco.png" alt="">
+            <span>Usuario Inactivo</span>
+            <div class="separador"></div>
+            <div class="items pointer">
+                <div class="itemUsuario" onclick="logIn()">
+                    <i class="bi bi-box-arrow-in-left"></i>
+                    <span class="">Identificarse</span>
+                </div>
+            </div>
+        </div>
+    `
+    mostrarUsuarioActivo()
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+
+    window.addEventListener('resize', cambioScreenSize);
+
+    cambioScreenSize()
+
+    mostrarUsuarioActivo()
+    
     cargarCarritoLocal()
+    
     mostrarProductos('db/productos.json','cajonProductos')
 
     var input = document.getElementById('campoBuscar');
